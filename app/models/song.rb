24951765f8b08d5d1214ff48a9c1089619509f25
Alex Cole <alexcole@u.boisestate.edu>
2015-12-06 23:54:01 -0700

    fixed error reporting for songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,9 +2,9 @@ class Song < ActiveRecord::Base
 	     belongs_to :user
 
 	     has_attached_file :song
+	       do_not_validate_attachment_file_type :song
 
   validates_attachment_presence :song
-  do_not_validate_attachment_file_type :song
 
 
 
