357062f86517db0222b1697ec3399853a5b4888a
Alex Cole <alexcole@u.boisestate.edu>
2015-12-06 23:44:46 -0700

    fixed error reporting for songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,8 +4,7 @@ class Song < ActiveRecord::Base
 	     has_attached_file :song
 
   validates_attachment_presence :song
-  validates_attachment_content_type :song, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
-
+  do_not_validate_attachment_file_type :song
 
 
 
