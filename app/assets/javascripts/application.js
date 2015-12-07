7e8208479c30c9eaf2bb8eae1415fc849df50785
Alex Cole <alexcole@u.boisestate.edu>
2015-12-06 23:12:13 -0700

    assets compile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,10 +24,3 @@ $(function() {
    });
 });
 
-
-$(function() {
-   $('.playTune').play()
-});
-
-
-
--- app/assets/javascripts/hamburgler.js
+++ app/assets/javascripts/hamburgler.js
@@ -9,3 +9,5 @@ var ready = function() {
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+
